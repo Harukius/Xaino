623ac197907ecc227bee98aef770d27fe8a603d3
Harukius <radisalmov@gmail.com>
2022-06-10 17:15:12 +0300

    del
    del

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-class (name, age, city, last_name, gender):
-
-    def __init__(self, last_name, name, age, city, gender):
-        self.name = name
-        self.age = age
-        self.last_name = last_name
-        self.city = city
-        self.gender = gender
-
-    def reg(name, last_name, age, city, gender):
-        reg_name = name
-        reg_age = age
-        reg_last_name = last_name
-        reg_city = city
-        reg_gender = gender
-
-    reg()
+import websockets
